f7c215bcae7201fa9204a4130f707e5b42bf50dd
ZOMini <zarikacr@gmail.com>
2022-01-07 00:55:20 +0300

    v 1.5

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+# Пришлось все поотключать - не запускалось при проверке на сервере.
+
 # from drf_yasg import openapi
 # from drf_yasg.views import get_schema_view
 # from rest_framework import permissions
